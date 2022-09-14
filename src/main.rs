be4af103163ca4b64877d630ceeb0aaf245f07fe
Nathan Douglas <github@darkdell.net>
2022-09-14 14:35:28 -0400

    Cleanup main.rs.

--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,8 @@
 #![allow(unused_imports)]
 
 #[macro_use]
-extern crate clap;
-#[macro_use]
-extern crate derivative;
-#[macro_use]
 extern crate function_name;
 pub use ::function_name::named;
-#[macro_use]
-extern crate lazy_static;
-#[macro_use]
-extern crate log;
-extern crate pretty_env_logger;
-#[macro_use]
-extern crate serde;
-#[macro_use]
-extern crate serde_json;
-#[macro_use]
-extern crate specs;
-extern crate uuid;
 
 pub use hornvale::*;
 
