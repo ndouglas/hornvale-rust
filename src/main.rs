92ebe9036850df2c5f0aa9f5b853d067902b8c40
Nathan Douglas <github@darkdell.net>
2022-09-14 14:44:33 -0400

    Tidying up main.rs

--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,21 @@ extern crate function_name;
 use clap::Parser;
 use pretty_env_logger::init as pretty_env_logger_init;
 
+use io::start_output;
+use message::start_message_spammer;
+use state::State;
+use tick::start_tick;
+
 use hornvale::*;
 
 #[named]
 fn main() {
   pretty_env_logger_init();
-  state::State::new();
-  io::start_output();
+  State::new();
+  start_output();
   let _args = cli::Arguments::parse();
-  message::start_message_spammer();
-  tick::start_tick();
+  start_message_spammer();
+  start_tick();
   // Main game loop.
   loop {
     io::read_input();
